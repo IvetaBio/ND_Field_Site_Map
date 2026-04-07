23d14deea4553af8b7bd9f8813cc47c9f59d9933
Iveta <ivetacasey@gmail.com>
2026-04-07 13:39:59 -0500

    turning my regular table into a spatial object

--- scripts/create_map.R
+++ scripts/create_map.R
@@ -1,1 +1,17 @@
 
+# Loading libraries
+library(sf)
+library(ggplot2)
+library(ggiraph)
+library(dplyr)
+library(maps)
+library(htmltools)
+
+Dickinson_and_Williston_sites <- read.csv("data/site_coordinates.csv")
+
+#now turning the regular table into a spatial object "sf"
+sites_sf <- st_as_sf(
+  Dickinson_and_Williston_sites,
+  coords = c("Longitude", "Latitude"),
+  crs = 4326 #Note: crs = coordinate reference system, based on GPS(WGS84), and units are in degrees
+)
